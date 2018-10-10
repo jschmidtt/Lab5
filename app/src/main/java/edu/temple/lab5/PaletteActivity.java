ad9c26629d906e23d1fd78690e9df2d54215c5b3
justinschmidt <justinschmidttt@gmail.com>
2018-10-10 10:23:51 -0400

    String Updates

--- app/src/main/java/edu/temple/lab5/PaletteActivity.java
+++ app/src/main/java/edu/temple/lab5/PaletteActivity.java
@@ -25,7 +25,7 @@ public class PaletteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_palette);
-        setTitle("Palette Activity");
+        setTitle(R.string.title_one);
 
         //Set Colors
         Resources res = getResources();
